99616b9b88c229e42a022f45214a8538f2410162
minefled <florian-martin@stembal.de>
2022-01-24 16:56:35 +0100

    Create Packet & config

---
+++ src/link/protocol/Packet.go
@@ -0,0 +1,82 @@
+package protocol
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type PacketField struct {
+	DataType string
+	Value    string
+}
+
+type Packet struct {
+	PType  string
+	Fields []PacketField
+}
+
+func ParsePacket(data string) *Packet {
+	p := Packet{}
+
+	/* === Raw String Handling === */
+
+	// Remove semicolon from the end of the raw data
+	if string(rune(data[len(data)-1])) == ";" {
+		data = data[:len(data)-1]
+	}
+
+	// Split the data into parts, seperated by a "|"
+	parts := strings.Split(data, " | ")
+
+	// Check if the packet has at least one part
+	if len(parts) < 1 {
+		return nil
+	}
+
+	/* === Analysing the raw data === */
+
+	// Get the Packet Type
+	p.PType = parts[0]
+	parts = parts[1:]
+
+	// Get all other (optional) fields
+	for i := 0; i < len(parts); i++ {
+		prt := parts[i]
+
+		DataType := ""
+		Value := prt
+
+		// Determine the Datatype
+		if canBeInt(prt) {
+			DataType = "int"
+		} else if canBeBool(prt) {
+			DataType = "bool"
+		} else {
+			DataType = "string"
+		}
+
+		p.Fields = append(p.Fields, PacketField{DataType, Value})
+	}
+
+	/* === Obviously, returning the result === */
+	fmt.Println(p)
+	return &p
+}
+
+/*
+ * Returns a boolean indicating whether the passed
+ * string can be converted to an integer
+ */
+func canBeInt(str string) bool {
+	_, err := strconv.Atoi(str)
+	return err == nil
+}
+
+/*
+ * Returns a boolean indicating whether the string
+ * can be converted to a boolean
+ */
+func canBeBool(str string) bool {
+	return strings.ToLower(str) == "false" || strings.ToLower(str) == "true"
+}
---
+++ src/system/config.go
@@ -0,0 +1,3 @@
+package system
+
+const SCAN_INTERVAL uint8 = 15
